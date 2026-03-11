9e5e42e627821ce690787265ccaa160e68e895bd
Portfolio User <portfolio@example.com>
2026-03-11 01:19:59 +0100

    Fix redirects to use lambdas avoiding import-time reverse

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.shortcuts import redirect
 
 urlpatterns = [
-    path('', redirect('home#home'), name='home'),
+    path('', lambda request: redirect('home#home'), name='home'),
     path('about/', lambda request: redirect('home#about'), name='about'),
     path('experiences/', lambda request: redirect('home#experience'), name='experiences'),
     path('contact/', lambda request: redirect('home#contact'), name='contact'),
