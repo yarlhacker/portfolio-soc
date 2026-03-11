47ec5f3f0c1288270145add3c6576f2ef44b2ee8
Portfolio User <portfolio@example.com>
2026-03-11 01:18:47 +0100

    Restore main/urls.py

--- main/urls.py
+++ main/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
 from django.shortcuts import redirect
-from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', redirect('home#home'), name='home'),
     path('about/', lambda request: redirect('home#about'), name='about'),
     path('experiences/', lambda request: redirect('home#experience'), name='experiences'),
     path('contact/', lambda request: redirect('home#contact'), name='contact'),
